2bb2da9bd058ea65828f8f102656596f5afe8612
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-04-01 23:09:00 -0500

    Basic functionalities: Append links and show them in a list

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push("www.awesomelead.com");
-  console.log(myLeads);
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+}
