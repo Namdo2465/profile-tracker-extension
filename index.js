40bf8dc99a43788a4fce4f374691ef6549cce1d7
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-04-01 23:50:50 -0500

    Add delete all button

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
+// Check if there are leads in local storage and if so, set myLeads to the stored leads and render them
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
 
+// Delete all leads from local storage on double click of the delete button
+deleteBtn.addEventListener("dblclick", function () {
+  console.log("double clicked!");
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
+
+// Add a new lead to the list and save it to local storage on click of the input button
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
@@ -17,6 +27,7 @@ inputBtn.addEventListener("click", function () {
   renderLeads();
 });
 
+// Render the list of leads in the unordered list element
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
