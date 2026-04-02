bb55ad283f73d34d250acce3ce427052c375bbac
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-04-01 23:42:21 -0500

    Create a local storage to store the links

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `
+           <li>
+               <a target='_blank' href='${myLeads[i]}'>
+                   ${myLeads[i]}
+               </a>
+           </li>
+       `;
   }
   ulEl.innerHTML = listItems;
 }
